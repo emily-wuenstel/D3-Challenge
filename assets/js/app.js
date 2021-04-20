736061413cb8a52c13bf33392aa060214d6f8068
emily-wuenstel <74880899+emily-wuenstel@users.noreply.github.com>
2021-04-20 17:12:41 -0500

    Update app.js

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,1 +1,16 @@
 // @TODO: YOUR CODE HERE!
+//Define SVG first to hold the chart and margins. Set height and width to adjust for margins
+
+var svgHeight = 960;
+var svgWidth = 500;
+
+var margin = {
+    top: 20,
+    right: 40,
+    bottom: 80, 
+    left: 100
+}
+
+var height = svgHeight - margin.left - margin.right;
+var width = svgWidth - margin.top - margin.bottom;
+
